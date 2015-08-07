b3370e7abe4fc776f7de2cf38a68c8f0015c63fb
Shirish <shirishdhar@berkeley.edu>
2015-08-07 08:56:29 -0700

    Exercise 6

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -6,7 +6,13 @@
 ################################################################################
 # Exercise 1
 # When you submit only include your final function: compare
-
+def compare(x,y):
+    if x>y:
+        print 1
+    elif x==y:
+        print 0
+    else:
+        print -1
 
 
 
@@ -15,14 +21,22 @@
 # Exercise 2
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share here.
-
-
+def hypotenuse(x,y):
+    import math
+    dsquared=x**2 + y**2
+    result = math.sqrt(dsquared)
+    print result
 
 
 
 ################################################################################
 # Exercise 3
 # When you submit only include your final function: is_between
+def is_between(x,y,z):
+    if (x<=y and y<=z):
+        print True
+    else:
+        print False
 
 
 
@@ -31,7 +45,14 @@
 ################################################################################
 # Exercise 6
 # When you submit only include your final function: is_palindrome
-
+def is_palindrome(s):
+    def inverse(s):
+        return s[::-1]
+    if (s==inverse(s)):
+        print('Pal')
+    else:
+        print('No')
+    
 
 
 
@@ -39,7 +60,12 @@
 ################################################################################
 # Exercise 7
 # When you submit only include your final function: is_power
-
+def is_power(a,b):
+    if(float(float(a)/float(b))%1==0 and float(float(a)/float(b))%b== 0):
+        print('Yes')
+    else:
+        print('no')
+    
 
 
 
@@ -60,28 +86,28 @@ def main():
     ############################################################################
     # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
-    # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
+    compare(1,1)
+    compare(1,2)
+    compare(2,1)
+    # Exercise 2
+    hypotenuse(1,1)
+    hypotenuse(3,4)
+    hypotenuse(1.2,12)
     # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
+    is_between(1,2,3)
+    is_between(2,1,3)
+    is_between(3,1,2)
+    is_between(1,1,2)
     # # Exercise 6
-    # is_palindrome("Python")
-    # is_palindrome("evitative")
-    # is_palindrome("sememes")
-    # is_palindrome("oooooooooooo")
+    is_palindrome("Python")
+    is_palindrome("evitative")
+    is_palindrome("sememes")
+    is_palindrome("oooooooooooo")
     # # Exercise 7
-    # is_power(28,3)
-    # is_power(27,3)
-    # is_power(248832,12)
-    # is_power(248844,12)
+    is_power(28,3)
+    is_power(27,3)
+    is_power(248832,12)
+    is_power(248844,12)
 
 
 if __name__ == "__main__":
